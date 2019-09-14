8f55224dca5aec885e832af9c92b24cf34e130c6
William Barkoff <william@barkoffusa.com>
2019-09-13 23:36:47 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,56 @@
+document.getElementById("date").innerText = new Date().toLocaleDateString('en-US', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' });
+
+setInterval(() => {
+    document.getElementById("time").innerText = new Date().toLocaleTimeString()
+}, 1000)
+
+window.onSpotifyWebPlaybackSDKReady = () => {
+    const token = propmt("What is your spotify token?");
+    const player = new Spotify.Player({
+        name: 'Assembly Site',
+        getOAuthToken: cb => { cb(token); }
+    });
+
+    // Error handling
+    player.addListener('initialization_error', ({ message }) => { console.error(message); });
+    player.addListener('authentication_error', ({ message }) => { console.error(message); });
+    player.addListener('account_error', ({ message }) => { console.error(message); });
+    player.addListener('playback_error', ({ message }) => { console.error(message); });
+
+    // Playback status updates
+    player.addListener('player_state_changed', state => {
+        console.log("STATE")
+        const track = state.track_window.current_track
+
+        console.log(track)
+
+        const artists = track.artists.map((artist, i) => {
+            return artist.name;
+        })
+
+        let formattedArtists;
+
+        if (artists.length > 1) {
+            const last = artists.pop();
+            formattedArtists = artists.join(', ') + ' and ' + last;
+        } else {
+            formattedArtists = artists[0]
+        }
+
+        document.getElementById("song-name").textContent = track.name;
+        document.getElementById("song-artist").textContent = formattedArtists;
+    });
+
+    // Ready
+    player.addListener('ready', ({ device_id }) => {
+        console.log('Ready with Device ID', device_id);
+    });
+
+    // Not Ready
+    player.addListener('not_ready', ({ device_id }) => {
+        console.log('Device ID has gone offline', device_id);
+    });
+
+    // Connect to the player!
+    player.connect();
+};
